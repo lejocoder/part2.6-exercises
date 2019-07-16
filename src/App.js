2faf32756b137fa6f1bf52d9225211ef6f166cb9
john le <jcle1@uci.edu>
2019-07-16 16:31:37 -0700

    part 2.7 dunnn

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React, { useState } from 'react'
-// 2.6
+// 2.7
 const App = () => {
   const [ persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) // we set persons with an intial array containing arto hellas
   const [ newName, setNewName ] = useState('') // newName is empty
-  const handleOnChange = (event) => {
-    console.log(event.target.value)
+  const handleOnChange = (event) => { // our event handler whenever 
+    //we change the typingbox  area
+    //console.log(event.target.value)
     setNewName(event.target.value)
   }
   const addName = (event) => {
-    event.preventDefault() 
-    const personObject = {
-      name: newName
+    event.preventDefault() // avoids the default 
+    //action that it usually does
+    const names = persons.map(person => person.name) // we map this so we can get a 
+    //proper array to work with, (no annyoing curly braces)
+    if (names.includes(newName)) { 
+      // use includes to check if newName is in the arrya,
+      // if it is put up an alert and reset the textbox by resetting 
+      //newName to ""
+      window.alert(`${newName} is already added to phonebook`)
+      setNewName('')
     }
-    const copy = persons // make a copy
-    copy.push(personObject) // push new name to the copy
-    setPersons(copy) // reassign copy to be the new persons array 
-    setNewName('') // reset to empty for the next new name
-  }
+    else {
+      const personObject = {
+        name: newName
+      }
+      const copy = persons // make a copy
+      copy.push(personObject) // push new name to the copy
+      setPersons(copy) // reassign copy to be the new persons array 
+      setNewName('') // reset to empty for the next new name
+    }
+    }
+    
   return (
     <div>
       <h2>Phonebook</h2>
